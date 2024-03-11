70043a56932ce11a18f5c73f9bfed87682b006bd
Anastasiia Drozd <a.drozd@spd-ukraine.com>
2024-03-11 16:17:20 +0200

    add external layout

---
+++ src/app/layouts/ExternalLayout/index.tsx
@@ -0,0 +1,36 @@
+import { useNavigate } from "react-router-dom";
+
+import { Button, Container, Stack, Typography } from "@mui/material";
+
+interface ExternalLayoutProps {
+  title: string;
+  description: string;
+}
+
+export default function ExternalLayout({
+  title,
+  description,
+}: ExternalLayoutProps) {
+  const navigate = useNavigate();
+
+  return (
+    <Container>
+      <Stack
+        spacing={2}
+        sx={{
+          margin: "auto",
+          textAlign: "center",
+          alignItems: "center",
+          justifyContent: "center",
+          maxWidth: 480,
+          minHeight: "100vh",
+        }}
+      >
+        <Typography variant="h4">{title}</Typography>
+        <Typography>{description}</Typography>
+
+        <Button onClick={() => navigate(-1)}>Go back</Button>
+      </Stack>
+    </Container>
+  );
+}
---
+++ src/app/layouts/constants.ts
@@ -0,0 +1,3 @@
+export const OPENED_SIDEBAR_WIDTH = 324;
+export const HEADER_HEIGHT = 80;
+export const MAIN_CONTAINER_PADDING = 3;
